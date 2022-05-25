c3fb81540b8afe616fd3d96196483a3a34e84461
Bruno Piffer <jogosdebruno123@gmail.com>
2022-05-25 14:36:20 -0300

    update code

--- app/Http/Controllers/MeasurementsController.php
+++ app/Http/Controllers/MeasurementsController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Measurements;
 
 class MeasurementsController extends Controller
 {
     //
+    public function store(Request $request){
+        //criando o objeto Medidas
+        $measurements = new Measurements();
+
+        //alterando os atributos do objeto
+        $measurements = $request->all();
+
+        //encaminhando para salvar no banco de dados
+        $measurements->save();
+    }
 }
--- app/Models/Measurements.php
+++ app/Models/Measurements.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class measurements extends Model
+class Measurements extends Model
 {
     use HasFactory;
 }
